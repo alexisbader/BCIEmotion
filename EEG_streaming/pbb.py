524977399adf66da70d6445765fc4f7627436c7f
alexisbader <79996302+alexisbader@users.noreply.github.com>
2024-02-14 18:08:12 -0500

    labels

--- EEG_streaming/pbb.py
+++ EEG_streaming/pbb.py
@@ -1,4 +1,5 @@
 import csv
+import time
 from neurosity import NeurositySDK
 from dotenv import load_dotenv
 import os
@@ -17,14 +18,22 @@ neurosity.login({
 # Define the CSV file path
 csv_file_path = "power_by_band_data.csv"
 
+# Constants
+NEUTRAL_DURATION = 12  # seconds
+EMOTION_DURATION = 46  # seconds
+CYCLES = 4  # number of cycles
+
 def callback(data):
     print("Received data")
 
     # Flatten the data
     flat_data = [data['label']] + [item for sublist in data['data'].values() for item in sublist]
 
+    # Add a column for the state ('neutral' or 'emotion')
+    flat_data.insert(0, 'neutral' if data['label'] == 'neutral' else 'emotion')
+
     # CSV header
-    header = ['label'] + [f'{band}_{i}' for band in data['data'].keys() for i in range(len(data['data'][band]))]
+    header = ['state'] + ['label'] + [f'{band}_{i}' for band in data['data'].keys() for i in range(len(data['data'][band]))]
 
     # Write to CSV in append mode
     with open('power_by_band_data.csv', 'a', newline='') as csv_file:
@@ -33,6 +42,22 @@ def callback(data):
         if os.stat('power_by_band_data.csv').st_size == 0:
             csv_writer.writerow(header)
         csv_writer.writerow(flat_data)
-        
-unsubscribe = neurosity.brainwaves_power_by_band(callback)
 
+# Repeat the cycle for the specified number of times
+for cycle in range(CYCLES):
+    print(f"Cycle {cycle + 1}/{CYCLES}")
+
+    # Collect 'neutral' data
+    print("Collecting neutral data...")
+    unsubscribe_neutral = neurosity.brainwaves_power_by_band(callback)
+    time.sleep(NEUTRAL_DURATION)
+    unsubscribe_neutral()
+
+    # Collect 'emotion' data
+    print("Collecting emotion data...")
+    unsubscribe_emotion = neurosity.brainwaves_power_by_band(callback)
+    time.sleep(EMOTION_DURATION)
+    unsubscribe_emotion()
+
+# Logout and stop the script
+neurosity.logout()

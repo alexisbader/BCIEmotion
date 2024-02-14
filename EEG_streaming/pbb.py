e72eea10c35d5d9820bd437c333f207d22a24b78
alexisbader <79996302+alexisbader@users.noreply.github.com>
2024-02-14 18:12:41 -0500

    label

--- EEG_streaming/pbb.py
+++ EEG_streaming/pbb.py
@@ -23,6 +23,8 @@ NEUTRAL_DURATION = 12  # seconds
 EMOTION_DURATION = 46  # seconds
 CYCLES = 4  # number of cycles
 
+state = "neutral"
+
 def callback(data):
     print("Received data")
 
@@ -30,7 +32,7 @@ def callback(data):
     flat_data = [data['label']] + [item for sublist in data['data'].values() for item in sublist]
 
     # Add a column for the state ('neutral' or 'emotion')
-    flat_data.insert(0, 'neutral' if data['label'] == 'neutral' else 'emotion')
+    flat_data.insert(0, 'neutral' if state == 'neutral' else 'emotion')
 
     # CSV header
     header = ['state'] + ['label'] + [f'{band}_{i}' for band in data['data'].keys() for i in range(len(data['data'][band]))]
@@ -48,12 +50,14 @@ for cycle in range(CYCLES):
     print(f"Cycle {cycle + 1}/{CYCLES}")
 
     # Collect 'neutral' data
+    state = "neutral"
     print("Collecting neutral data...")
     unsubscribe_neutral = neurosity.brainwaves_power_by_band(callback)
     time.sleep(NEUTRAL_DURATION)
     unsubscribe_neutral()
 
     # Collect 'emotion' data
+    state = "emotion"
     print("Collecting emotion data...")
     unsubscribe_emotion = neurosity.brainwaves_power_by_band(callback)
     time.sleep(EMOTION_DURATION)
